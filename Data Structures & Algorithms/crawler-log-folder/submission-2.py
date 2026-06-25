a531918f7041f5d58c4413605f4ebb2cdd94c174
Bonnie Chen <bonniec0819@gmail.com>
2026-06-25 10:52:38 -0400

    Add: crawler-log-folder - submission-2

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = [];
+        for i in logs:
+            if(i == "../"):
+                if(len(stack) != 0):
+                    stack.pop()
+                continue
+            elif(i == "./"):
+                continue
+            else:
+                stack.append(i);
+        return len(stack)
